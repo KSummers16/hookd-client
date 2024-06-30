ad18f7296a568a0b5ad78303d2b0b8fa1a2d0980
Kimberly Summers <kasummers16@gmail.com>
2024-06-29 19:44:54 -0500

    fixing vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+    minify: "terser",
+  },
+  // Server config for both dev and preview
   server: {
-    hmr: {
-      overlay: false,
-    },
+    host: true, // This listens on all available network interfaces
+    port: process.env.PORT || 3000,
   },
 })
