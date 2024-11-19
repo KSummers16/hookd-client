709f2bffabc52e5730d9fa04409cdcf160ec3cd0
Kimberly Summers <kasummers16@gmail.com>
2024-11-19 12:11:15 -0600

    more yarn stash

--- src/assets/components/yarnform.jsx
+++ src/assets/components/yarnform.jsx
@@ -6,7 +6,7 @@ import {
 } from "../managers/productmanager.jsx"
 import { AddNewCustomerYarn } from "./newyarnstash.jsx"
 
-export const YarnForm = ({ currentUser }) => {
+export const YarnForm = ({ currentUser, onCloseForm }) => {
   const [request, setRequest] = useState({
     customer_id: currentUser.id,
     name: "",
@@ -36,8 +36,55 @@ export const YarnForm = ({ currentUser }) => {
     fetchOptions()
   }, [id])
 
+  const handleChange = (event) => {
+    const copy = { ...request }
+    const { id, value } = event.target
+
+    if (id === "amount") {
+      copy[id] = parseFloat(value)
+    } else if (
+      id === "base_color_id" ||
+      id === "weight_id" ||
+      id === "copmany_id"
+    ) {
+      copy[id] = parseInt(value)
+    } else {
+      copy[id] = value
+    }
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    AddNewCustomerYarn(request).then(() => {})
+    AddNewCustomerYarn(request).then(() => {
+      onCloseForm()
+    })
   }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <div>
+            <label htmlFor="weight">Weight</label>
+            <select
+              required
+              id="weight_id"
+              value={request.weight_id}
+              onChange={handleChange}
+            >
+              <option value="">Select a weight</option>
+              {weights.map((weight) => (
+                <option key={weight.id} value={weight.id}>
+                  {weight.id}.{weight.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label></label>
+          </div>
+        </fieldset>
+      </form>
+    </>
+  )
 }
